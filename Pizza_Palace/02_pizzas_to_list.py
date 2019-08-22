4b4200f37b5ec0bc044c64a84cca01bb1e057d77
Uzukira <53543490+Uzukira@users.noreply.github.com>
2019-08-22 13:18:22 +1200

    Update 02_pizzas_to_list.py

--- Pizza_Palace/02_pizzas_to_list.py
+++ Pizza_Palace/02_pizzas_to_list.py
@@ -4,4 +4,4 @@ def pizza_list(question):
 
     error = "Your input does not match the available options"
 
-    available_pizzas = ["Hawaiian", "Margarita", "Meat lovers", "Cheesy"]
+    available_pizzas = ["Hawaiian", "Margarita", "Meat lovers", "Classic Cheese", "Pepperoni", "Ham and Cheese", "Beef and Onion", ""]
